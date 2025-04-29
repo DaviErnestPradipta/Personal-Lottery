1bd880dda261d69eb6314e04ecc85fc63a16802a
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-29 12:56:40 +0900

    Add 2023.js

---
+++ src/year/2023.js
@@ -0,0 +1,24 @@
+export const order = [
+    "DET",
+    "HOU",
+    "SAS",
+    "CHA",
+    "POR",
+    "ORL",
+    "IND",
+    "WAS",
+    "UTA",
+    "DAL",
+    "CHI",
+    "OKC",
+    "TOR",
+    "NOP",
+];
+
+export const change = [
+    [9, 0, 9, "DAL>NYK"],
+    [10, 0, 3, "CHI>ORL"],
+];
+
+export const chance = [140, 140, 140, 125, 105, 90, 68, 67, 45, 30, 18, 17, 10, 5];
+export const lotteryTeams = 4;
