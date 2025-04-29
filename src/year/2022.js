76760d2ad22ce1d8d9137c4dcf8e22eb4bf030f0
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-29 13:14:28 +0900

    Add 2022.js

---
+++ src/year/2022.js
@@ -0,0 +1,24 @@
+export const order = [
+    "HOU",
+    "ORL",
+    "DET",
+    "OKC",
+    "IND",
+    "POR",
+    "SAC",
+    "LAL>NOP",
+    "SAS",
+    "WAS",
+    "NYK",
+    "LAC>OKC",
+    "CHA",
+    "CLE",
+];
+
+export const change = [
+    [7, 0, 9, "LAL>MEM"],
+    [7, 0, 11, "LAL"],
+];
+
+export const chance = [140, 140, 140, 125, 105, 90, 75, 60, 45, 30, 20, 15, 10, 5];
+export const lotteryTeams = 4;
