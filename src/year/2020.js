1ff0f282d85253bc8f469e487e815122407e03dc
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-29 13:43:30 +0900

    Add 2020.js

---
+++ src/year/2020.js
@@ -0,0 +1,28 @@
+export const order = [
+    "GSW",
+    "CLE",
+    "MIN",
+    "ATL",
+    "DET",
+    "NYK",
+    "CHI",
+    "CHA",
+    "WAS",
+    "PHX",
+    "SAS",
+    "SAC",
+    "NOP",
+    "MEM",
+];
+
+// Index of original pick owner
+// Index of upper limit
+// Index of lower limit
+// Name of new pick owner if pick falls outside the limits
+
+export const change = [
+    [13, 0, 12, "MEM>BOS"],
+];
+
+export const chance = [140, 140, 140, 125, 105, 90, 75, 60, 45, 30, 20, 13, 12, 5];
+export const lotteryTeams = 4;
